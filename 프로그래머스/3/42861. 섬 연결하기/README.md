b5888522aea0227edbcc114e4d7dbe9b68e4aab1
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-06-15 16:36:22 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.03 ms
+메모리: 11.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 14일 12:33:00
+2026년 06월 15일 16:36:18
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -4,43 +4,32 @@ def solution(n, costs):
     # 아 이거 손으로 하면 엄청 쉬운데
     # 코드로 하려니까 막막하네
     parent = [i for i in range(n)]
-    rank=[0] * n
+    rank = [0] * n
     def find(x):
-        root = x
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
         
-        while parent[root] != root:
-            root = parent[root]
-        
-        while parent[x] != x:
-            next_x = parent[x]
-            parent[x] = root
-            x = next_x
-        return root
     def union(a,b):
         root_a = find(a)
         root_b = find(b)
-        
         if root_a == root_b:
             return False
-        
-        if rank[root_a] < rank[root_b]:
-            parent[root_a] = root_b
-        elif rank[root_a] > rank[root_b]:
-            parent[root_b] = root_a
-        else:
-            parent[root_b] = root_a
-            rank[root_a] +=1
+        parent[root_a] = root_b
         return True
-    costs.sort(key=lambda x:x[2])
-    
+        
+        
+        
     answer = 0
     edge_count = 0
-    
+    costs.sort(key = lambda x:x[2])
     for a, b, cost in costs:
         if union(a,b):
-            answer += cost
             edge_count += 1
-        
-        if edge_count == n-1:
-            break
+            answer += cost
+            if edge_count == n-1:
+                break
     return answer
+
+        
+            
