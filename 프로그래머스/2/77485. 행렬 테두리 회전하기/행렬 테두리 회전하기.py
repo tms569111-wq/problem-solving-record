d83efe38f56e25d5437b3d2eb44165fae9068fc3
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-06-15 22:36:02 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 124.63 ms, Memory: 12.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -0,0 +1,38 @@
+def solution(rows, columns, queries):
+    arr = [[i for i in range(j*columns+1,j*columns+columns+1)] for j in range(0,rows)]
+    answer = []
+    min_num = 0
+    def clock_wise_turn(x1, y1, x2, y2, arr):
+        temp = 0
+        now = arr[x1][y1]
+        check = 0
+        dx = [0, 1, 0 ,-1]
+        dy = [1, 0, -1, 0]
+        dx_now = x1
+        dy_now = y1
+        count = 0
+        list_min = 1e9
+        move_count = 2 * ((x2 - x1) + (y2 - y1))
+        while count < move_count:
+            list_min = min(now, list_min)
+            check_x = dx_now + dx[check]
+            check_y = dy_now + dy[check]
+            
+            if check_x < x1 or check_x > x2 or check_y < y1 or check_y > y2:
+                check += 1
+                check %= 4
+                dx_now = dx_now + dx[check]
+                dy_now = dy_now + dy[check]
+            else:
+                dx_now = check_x 
+                dy_now = check_y
+            temp = arr[dx_now][dy_now]
+            arr[dx_now][dy_now] = now
+            now = temp
+            count += 1
+        return arr, list_min
+    for query in queries:
+        x1, y1, x2, y2 = query[0]-1, query[1]-1, query[2]-1, query[3]-1
+        arr, min_num = clock_wise_turn(x1, y1, x2, y2, arr)
+        answer.append(min_num)
+    return answer
