ebb1480de74bd96b51ffc95f3c114cc3d885b469
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-06 21:21:40 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 0.01 ms
+메모리: 11.7 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 31일 07:21:58
+2026년 09월 06일 21:21:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,9 @@
 from collections import defaultdict
-from itertools import combinations
 def solution(clothes):
-    clothes_dic = defaultdict(int)
+    dic = defaultdict(lambda : 1)
     for i in range(len(clothes)):
-        clothes_dic[clothes[i][1]] += 1
-    answer = 1
-    for i in clothes_dic.values():
-        answer *= (i + 1)
-        
-    return answer - 1
+        dic[clothes[i][1]] += 1
+    count = 1
+    for value in dic.values():
+        count *= value
+    return count - 1
