7d80bff1d64576f2feee79e8439ffc16bc3ad802
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-06 20:13:51 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,3 @@
 from collections import defaultdict
 def solution(nums):
-    dic = defaultdict(int)
-    check = len(nums) // 2
-    for i in nums:
-        dic[i] +=1
-    count = 0
-    for key in dic.keys():
-        count += 1
-        if count >= check:
-            return count
-    return count
+    return min(len(nums)//2, len(set(nums)))
