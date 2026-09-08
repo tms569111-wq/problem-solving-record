cb067b8bbe64f499432b1cfe9268a4f17501dd86
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-08 16:59:32 +0900

    [level 2] Title: 문자열 압축, Time: 1.04 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 0.88 ms
+메모리: 11.3 MB, 시간: 1.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 22일 13:18:22
+2026년 09월 08일 16:59:28
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,32 +1,26 @@
 def solution(s):
-    # len(s) //2 보다 더 커지면 의미가 없음
-    # 시간복잡도로 보면 1000 * 1000 100만 정도인가
-    # 앞 뒤로 i 크기만큼 잘라서 비교해서
-    # 만약 같으면 num+=1
-    # str(num) + last
-    answer = len(s)
-    
-    for unit in range(1, len(s)//2 + 1):
-        compressed = ""
-        
-        prev = s[0:unit]
-        count =1 
-        for i in range(unit, len(s), unit):
-            cur = s[i:i+unit]
-            if cur == prev:
-                count += 1
+    n = len(s)
+    answer = 1e19
+    if n <= 2:
+        return n
+    for i in range(1, n // 2 + 1):
+        previous = s[:i]
+        pre_count = 1
+        now = ""
+        for j in range(i, n, i):
+            new = s[j:i + j]
+            if new != previous:
+                if pre_count >= 2:
+                    now += str(pre_count)
+                now += previous
+                previous = new
+                pre_count = 1
             else:
-                if count == 1:
-                    compressed += prev
-                else:
-                    compressed += str(count) + prev
-                prev = cur
-                count = 1
-        
-        if count ==1:
-            compressed += prev
-        else:
-            compressed += str(count) + prev
-        answer = min(answer, len(compressed))
+                pre_count += 1
+            
+        if pre_count >= 2:
+            now += str(pre_count)
+
+        now += previous
+        answer = min(len(now), answer)
     return answer
-   
