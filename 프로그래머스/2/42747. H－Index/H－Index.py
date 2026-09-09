1670427c853eec5c999b2a5ce7be082daff707c2
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-09 20:34:45 +0900

    [level 2] Title: H-Index, Time: 0.24 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,14 @@
+# 최대 1000번
+from collections import defaultdict
 def solution(citations):
-    citations.sort(reverse=True)
+    length = len(citations)
+    citations.sort()
     answer = 0
+    dic = defaultdict(int)
     
-    for i in range(len(citations)):
-        if citations[i]>=(i+1):
-            continue
-        else:
-            return i
-    
-    return len(citations)
+    for i in range(length):
+        if length - i <= citations[i]:
+            dic[citations[i]] = length - i
+    for value in dic.values():
+        answer = max(answer, value)
+    return answer
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 0.15 ms
+메모리: 11.7 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 09일 13:02:29
+2026년 09월 09일 20:34:41
 
 ### 문제 설명
 
