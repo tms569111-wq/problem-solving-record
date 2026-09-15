89f89d29fd92bc069d484b3d92a30c5a0a561ae9
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-15 15:32:09 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 107.22 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -3,26 +3,23 @@ def solution(rows, columns, queries):
     board = [[0 for _ in range(columns)] for _ in range(rows)]
     def rotate_border(board, x1, y1, x2, y2, k = 1):
         pos = []
-        
         for y in range(y1, y2 + 1):
             pos.append((x1, y))
-
+        
         for x in range(x1 + 1, x2 + 1):
             pos.append((x, y2))
         
-        for y in range(y2 - 1, y1 - 1, -1):
+        for y in range(y2 - 1, y1 - 1, - 1):
             pos.append((x2, y))
         
         for x in range(x2 - 1, x1, -1):
             pos.append((x, y1))
         q = deque(board[x][y] for x, y in pos)
+        q.rotate()
         
-        q.rotate(k)
-        min_value = 1e9
         for (x, y), value in zip(pos, q):
             board[x][y] = value
-            min_value = min(value, min_value)
-        return min_value
+        return min(q)
     count = 1
     for i in range(rows):
         for j in range(columns):
