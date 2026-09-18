5609a0b53a3706282d2f84548202b4192581969e
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-18 21:17:28 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 30.10 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,35 +1,45 @@
-from functools import lru_cache
+import heapq
 def solution(board):
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
     n = len(board)
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    fee = [[1e9 for _ in range(n)] for _ in range(n)]
+    dist = [[[int(1e9) for _ in range(4)] for _ in range(n)] for _ in range(n)]
+    heap = []
     
-    @lru_cache(None)
-    def dfs(x, y, money, previous):
+    dist[0][0][1] = 100
+    heapq.heappush(heap, (100, 0, 0, 1))
+    dist[0][0][3] = 100
+    heapq.heappush(heap, (100, 0, 0, 3))
+
+        
+    while heap:
+        cost, x, y, d = heapq.heappop(heap)
+        
+        if dist[x][y][d] < cost:
+            continue
+            
         if x == n - 1 and y == n - 1:
-            if money < fee[n - 1][n - 1]:
-                fee[n - 1][n - 1] = money
-            return
+            continue
+        
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or nx >= n or ny < 0 or ny >= n or board[nx][ny] == 1:
                 continue
-            if visited[nx][ny] == True:
-                continue
-            if board[nx][ny] == 1:
-                continue
-            if previous != i:
-                visited[nx][ny] = True
-                dfs(nx, ny, money + 500 + 100, i)
-                visited[nx][ny] = False
+                
+            if i == d:
+                new_cost = cost + 100
             else:
-                visited[nx][ny] = True
-                dfs(nx, ny, money + 100, i)
-                visited[nx][ny] = False
-                        
-    visited[0][0] = True
-    dfs(0, 0, -500, -1)
-    return fee[n - 1][n - 1]
+                new_cost = cost + 600
+                
+            if new_cost > dist[nx][ny][i]:
+                continue
+            else:
+                dist[nx][ny][i] = new_cost
+                heapq.heappush(heap, (new_cost, nx, ny, i))
+        
+                    
+    return min(dist[n-1][n-1]) - 100
+                    
+            
