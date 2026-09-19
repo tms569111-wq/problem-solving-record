fae20deff292f8de44855a5fa8c94ba4d785947d
유성민(DGIST 2026년 졸업) <tms569111@gmail.com>
2026-09-19 20:38:46 +0900

    [level 3] Title: 합승 택시 요금, Time: 24.61 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=python3) 
 
 ### 성능 요약
 
-메모리: 15.2 MB, 시간: 667.59 ms
+메모리: 16.8 MB, 시간: 24.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 27일 23:56:07
+2026년 09월 19일 20:38:42
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,25 +1,42 @@
+import heapq
+from collections import defaultdict
+# 그냥 a최소 b최소에서 가는 길 같을 때까지 더하는 건 애매한 게
+# a입장에서 살짝 돌고 b입장에서 살짝 돌아도
+# 합쳐서 얘네가 갔다가 내리면 이득인 부분이 존재 가능함
+# 어떤지점에서 a로 가는 최소 거리, b로 가는 최소 거리 그리고 s로 가는 최소 거리
+# 이 3개를 다 더한 거리
+# 가 최소인 점이 정답일 것 같은데...
+# 이걸 각각 모든 점에서 하는 것보다
+# 차라리 start를 a랑 b로 해서 a_dist, b_dist, s_dist하면 될듯.
+# 그뒤 min값을 구한다면?
 def solution(n, s, a, b, fares):
-    # 1. 합승하는 경우
-    # 1-1 같이 가다가 중간에 내리는 경우
-    # 1-2 도착점까지는 같이 가는 경우
-    # 합승 안하는 경우 비교
-    # 일단 s에서 그냥 a가는 경우 최솟값, s에서 그냥 b가는 경우 최소값 다익스트라나 다른 걸로 구하고
-    # 합승이 문제 합승은 일단 a랑 b에서 겹치는 부분까지는 같이 갔다가 달라지는 부분부터 최솟값비교?
+    graph = defaultdict(list)
+    for i, j, k in fares:
+        graph[i].append((j, k))
+        graph[j].append((i, k))
+    answer = 0
     
-    INF = 10**15
-    dist = [[INF] * (n + 1) for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        dist[i][i] = 0
-    for c, d, f in fares:
-        dist[c][d] = f
-        dist[d][c] = f
-    for mid in range(1, n + 1):
-        for start in range(1, n + 1):
-            for end in range(1, n + 1):
-                dist[start][end] = min(dist[start][end], dist[start][mid] + dist[mid][end])
-    answer = INF
-    for i in range(1, n + 1):
-        cost = dist[s][i] + dist[i][a] +dist[i][b]
-        answer = min(answer, cost)
-    
+    def dijkstra(start):
+        heap = []
+        heapq.heappush(heap, (0, start))
+        dist = [1e9] * (n + 1)
+        dist[start] = 0
+        while heap:
+            cost, now = heapq.heappop(heap)
+            if cost > dist[now]:
+                continue
+            for nxt, weight in graph[now]:
+                new = weight + cost
+                if dist[nxt] <= new:
+                    continue
+                dist[nxt] = new
+                heapq.heappush(heap, (new, nxt))
+        return dist
+    sum_dist = []
+    sum_dist.append(dijkstra(b))
+    sum_dist.append(dijkstra(a))
+    sum_dist.append(dijkstra(s))
+    answer = 1e9
+    for i in range(n + 1):
+        answer = min(answer, sum_dist[0][i] + sum_dist[1][i] + sum_dist[2][i])
     return answer
